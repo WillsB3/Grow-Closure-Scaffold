38dcc06ea6a08f2a60c73efcd819c493f3967940
Stu Cox <stucox@users.noreply.github.com>
2016-05-04 10:39:48 +0100

    Remove unnecessary `getTask` abstraction

--- gulp/utils/get-task.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var path = require('path');
-
-module.exports = function(name) {
-  var p = path.join(__dirname, '..', '..', 'gulp', 'tasks', name);
-  return require(p)(gulp);
-};
