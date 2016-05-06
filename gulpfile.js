766adab8e90f84311de65ba98cdfc4629037e5b0
Wills Bithrey <wbithrey@google.com>
2016-05-06 10:18:48 +0100

    Fix tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 
 // Initialise gulp tasks.
-gulp.task('clean', require('./gulp/tasks/clean'));
-gulp.task('closure-deps', require('./gulp/tasks/closure-deps'));
-gulp.task('compile-js', require('./gulp/tasks/compile-js'));
-gulp.task('lint-js', require('./gulp/tasks/lint-js'));
-gulp.task('lint-scss', require('./gulp/tasks/lint-scss'));
-gulp.task('preview', require('./gulp/tasks/preview'));
-gulp.task('sass', require('./gulp/tasks/sass'));
-gulp.task('symlink:dev', require('./gulp/tasks/symlink').dev);
-gulp.task('watch', require('./gulp/tasks/watch'));
+gulp.task('clean', require('./gulp/tasks/clean')(gulp));
+gulp.task('closure-deps', require('./gulp/tasks/closure-deps')(gulp));
+gulp.task('compile-js', require('./gulp/tasks/compile-js')(gulp));
+gulp.task('lint-js', require('./gulp/tasks/lint-js')(gulp));
+gulp.task('lint-scss', require('./gulp/tasks/lint-scss')(gulp));
+gulp.task('preview', require('./gulp/tasks/preview')(gulp));
+gulp.task('sass', require('./gulp/tasks/sass')(gulp));
+gulp.task('symlink:dev', require('./gulp/tasks/symlink')(gulp).dev);
+gulp.task('watch', require('./gulp/tasks/watch')(gulp));
 
 // Main tasks
-gulp.task('build', require('./gulp/tasks/build'));
-gulp.task('default', require('./gulp/tasks/default'));
+gulp.task('build', require('./gulp/tasks/build')(gulp));
+gulp.task('default', require('./gulp/tasks/default')(gulp));
